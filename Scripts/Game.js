2e75c066838878c0acd48604be3324bc01bb67c1
luke <lakehoffmann@gmail.com>
2014-09-14 14:01:46 +0930

    Replacement of pieces works
    Game basically plays as before without detecting endgame

--- Scripts/Game.js
+++ Scripts/Game.js
@@ -16,7 +16,7 @@ Q.SPRITE_PIECE = 21;
 Q.SPRITE_BOUND = 22;
 
 Q.touch(Q.SPRITE_PIECE);
-
+Q.gravityY = 5000;
 Q.Sprite.extend("Bound", {
     init: function (p) {
         this._super(p, {
@@ -54,13 +54,8 @@ Q.Sprite.extend("Piece", {
     
     touch: function () {
         this.p.onTouch(this);
-    },
-    
-    moveTo: function(column, row) {
-        this.p.column = column;
-        this.p.row = row;
-        return this;
     }
+    
 });
 
 Q.scene("gameboard", function (stage) {
@@ -68,65 +63,65 @@ Q.scene("gameboard", function (stage) {
         asset: "Tree.png",
         x: Q.el.width / 2,
         y: Q.el.height / 2,
-        type: Q.SPRITE_NONE,
+        type: Q.SPRITE_NONE
     }));
 
     var pieces = {
         _p: [],
 
         exists: function (c, r) {
-            return this._p[c] && this._p[c][r];
+            return this.piece(c, r) !== null;
         },
 
         piece: function (c, r) {
-            if (this.exists(c, r)) {
-                return this._p[c][r];
-            } else {
-                return null;
+            for (var i = 0; i < this._p.length; i++) {
+                if (this._p[i].p.column == c && this._p[i].p.row == r) {
+                    return this._p[i];
+                }
             }
+            return null;
         },
 
-        insert: function (c, r, piece) {
-            if (!this._p[c]) {
-                this._p[c] = [];
-            }
-            this._p[c][r] = piece;
+        add: function (piece) {
+            this._p.push(piece);
         },
 
-        clear: function (c, r) {
-            this._p[c][r] = null;
+        remove: function (piece) {
+            var index = this._p.indexOf(piece);
+            if (index > -1) {
+                this._p.splice(index, 1);
+            }
         }
     };
 
     var destroyPiece = function (piece) {
         var match = [], m;
-        adjacentMatch(piece.p.column, piece.p.row, match);
+        adjacentMatch(piece, match);
         if (match.length >= minimumMatch) {
             for (m = match.length - 1; m >= 0; m--) {
+                pieces.remove(match[m]);
                 match[m].destroy();
-                pieces.clear(match[m].p.column, match[m].p.row);
             }
         }
             
         dropColumns();
     };
     
-    var adjacentMatch = function (column, row, match, color) {
-                    
-        if (pieces.exists(column, row)) {
-            var piece = pieces.piece(column, row);
+    var adjacentMatch = function (piece, match, color) {
+        var column = piece.p.column,
+            row = piece.p.row;
             color = color || piece.p.color;
-        
-            if (piece && piece.p.color === color) {
-                if (match.indexOf(piece) < 0) {
-                    match.push(piece);
-                    adjacentMatch(column, row - 1, match, color);
-                    adjacentMatch(column, row + 1, match, color);
-                    adjacentMatch(column - 1, row, match, color);
-                    adjacentMatch(column + 1, row, match, color);
-                }
+    
+        if (piece && piece.p.color === color) {
+            if (match.indexOf(piece) < 0) {
+                match.push(piece);
+                if (pieces.exists(column, row - 1)) {adjacentMatch(pieces.piece(column, row - 1), match, color);}
+                if (pieces.exists(column, row + 1)) {adjacentMatch(pieces.piece(column, row + 1), match, color);}
+                if (pieces.exists(column - 1, row)) {adjacentMatch(pieces.piece(column - 1, row), match, color);}
+                if (pieces.exists(column + 1, row)) {adjacentMatch(pieces.piece(column + 1, row), match, color);}
             }
         }
+        
        return match;
     };
 
@@ -135,7 +130,22 @@ Q.scene("gameboard", function (stage) {
         for (c = 0; c < columns; c++) { //each colun
             for (r = rows - 1; r >= 0; r--) { //start from the bottom up
                 if (!pieces.exists(c, r)) {
-                    pieces.insert(c, r, pieceAbove(c, r).moveTo(c, r));
+                    var piece = pieceAbove(c, r);
+                    if (piece) {
+                        piece.p.row = r;
+                    } else {
+                        piece = stage.insert(
+                            new Q.Piece({
+                                color: randomColor(),
+                                onTouch: destroyPiece,
+                                column: c,
+                                row: r,
+                                x: pieceSize * (c + 1/2),
+                                y: pieceSize * (r - 1/2 - rows)
+                            })
+                        );
+                        pieces.add(piece);
+                    }
                 }
             }
         }
@@ -145,41 +155,31 @@ Q.scene("gameboard", function (stage) {
         var r, p;
         for (r = row; r >= 0; r--) {
             if (pieces.exists(column, r)) {
-                p = pieces.piece(column, r);
-                pieces.clear(column, r);
-                return p;
+                return pieces.piece(column, r);
             }
         }
-        return stage.insert(
-            new Q.Piece({
-                color: randomColor(),
-                onTouch: destroyPiece,
-                column: column,
-                row: row,
-                x: pieceSize * c + pieceSize / 2,
-                y: -pieceSize * row
-            })
-        );
+        return null;
     };
 
-    var c, r;
+    var c, r, piece;
     for (c = 0; c < columns; c++) {
         for (r = 0; r < rows; r++) {
-            pieces.insert(c, r,
-                stage.insert(new Q.Piece({
+            piece = stage.insert(new Q.Piece({
                     color: randomColor(),
                     onTouch: destroyPiece,
                     column: c,
                     row: r,
-                    x: pieceSize * c + pieceSize / 2,
-                    y: pieceSize * r + pieceSize / 2
-                }))
-            );
+                    x: pieceSize * (c + 1 / 2),
+                    y: pieceSize * (r - 1)
+                }));
+            //piece.p.gravity = 10;
+            pieces.add(piece);
         }
     }
+ 
     stage.insert(new Q.Bound({
-        y: (pieceSize * rows) + 10,
-        x: (pieceSize * columns) / 2
+        x: (pieceSize * columns) / 2,
+        y: (pieceSize * rows) + 10
     }));
     
 });
