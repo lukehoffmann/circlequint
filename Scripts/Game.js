87f7dd9e2a3fb600d5b8ca462bb894897f9383ee
Luke Hoffmann <luke.hoffmann@2icsoftware.com>
2014-09-16 17:05:42 +0930

    refactor, better colors

--- Scripts/Game.js
+++ Scripts/Game.js
@@ -2,7 +2,10 @@
 var columns = 3,
         rows = 6,
         pieceSize = 70, //px
-        colors = ['red', 'orange', 'pink', 'purple'],
+        colors = ['#D8608C',
+                  '#A5E164',
+                  '#F8EA6E',
+                  '#9D81C8'],
         minimumMatch = 3;
 
 var Q = Quintus()
@@ -12,16 +15,16 @@ var Q = Quintus()
         height: rows * pieceSize
     });
 
-Q.SPRITE_PIECE = 21;
-Q.SPRITE_BOUND = 22;
-
-Q.touch(Q.SPRITE_PIECE);
+Q.touch(Q.SPRITE_DEFAULT);
 Q.gravityY = 5000;
-Q.Sprite.extend("Bound", {
+
+Q.Sprite.extend("LowerBound", {
     init: function (p) {
         this._super(p, {
-            type: Q.SPRITE_PIECE,
+            type: Q.SPRITE_DEFAULT,
             color: 'white',
+            x: (pieceSize * columns) / 2,
+            y: (pieceSize * rows) + 10,
             w: pieceSize * columns,
             h: 20
         });
@@ -29,7 +32,24 @@ Q.Sprite.extend("Bound", {
 
     draw: function (ctx) {
         ctx.fillStyle = this.p.color;
-        // Draw a filled rectangle centered at 0,0 (i.e. from -w/2,-h2 to w/2, h/2)
+        ctx.fillRect(-this.p.cx, -this.p.cy, this.p.w, this.p.h);
+    }
+});
+
+Q.Sprite.extend('Background', {
+    init: function (p) {
+        this._super(p, {
+            type: Q.SPRITE_NONE,
+            w: Q.el.width,
+            h: Q.el.height,
+            x: Q.el.width / 2,
+            y: Q.el.height / 2,
+            color: '#111'            
+        });
+    },
+    
+    draw: function (ctx) {
+        ctx.fillStyle = this.p.color;
         ctx.fillRect(-this.p.cx, -this.p.cy, this.p.w, this.p.h);
     }
 });
@@ -37,9 +57,10 @@ Q.Sprite.extend("Bound", {
 Q.Sprite.extend("Piece", {
     init: function (p) {
         this._super(p, {
-            type: Q.SPRITE_PIECE,
+            type: Q.SPRITE_DEFAULT,
             w: pieceSize,
-            h: pieceSize
+            h: pieceSize,
+            color: colors[Math.floor(Math.random() * (colors.length - 0.9))]
         });
         this.on('touch');
         this.add("2d");
@@ -55,139 +76,131 @@ Q.Sprite.extend("Piece", {
     touch: function () {
         this.p.onTouch(this);
     }
-    
 });
 
-Q.scene("gameboard", function (stage) {
-    stage.insert(new Q.Sprite({
-        asset: "Tree.png",
-        x: Q.el.width / 2,
-        y: Q.el.height / 2,
-        type: Q.SPRITE_NONE
-    }));
+Q.Class.extend("PieceCollection", {
+    _p: [],
 
-    var pieces = {
-        _p: [],
+    exists: function (c, r) {
+        return this.piece(c, r) !== null;
+    },
 
-        exists: function (c, r) {
-            return this.piece(c, r) !== null;
-        },
-
-        piece: function (c, r) {
-            for (var i = 0; i < this._p.length; i++) {
-                if (this._p[i].p.column == c && this._p[i].p.row == r) {
-                    return this._p[i];
-                }
-            }
-            return null;
-        },
-
-        add: function (piece) {
-            this._p.push(piece);
-        },
-
-        remove: function (piece) {
-            var index = this._p.indexOf(piece);
-            if (index > -1) {
-                this._p.splice(index, 1);
+    piece: function (c, r) {
+        for (var i = 0; i < this._p.length; i++) {
+            if (this._p[i].p.column === c && this._p[i].p.row === r) {
+                return this._p[i];
             }
         }
-    };
+        return null;
+    },
 
-    var destroyPiece = function (piece) {
-        var match = [], m;
-        adjacentMatch(piece, match);
-        if (match.length >= minimumMatch) {
-            for (m = match.length - 1; m >= 0; m--) {
-                pieces.remove(match[m]);
-                match[m].destroy();
-            }
+    add: function (piece) {
+        this._p.push(piece);
+    },
+
+    remove: function (piece) {
+        var index = this._p.indexOf(piece);
+        if (index > -1) {
+            this._p.splice(index, 1);
+            piece.destroy();
         }
-            
-        dropColumns();
-    };
-    
-    var adjacentMatch = function (piece, match, color) {
+    },
+                
+    adjacentMatch: function (piece, match, color) {
         var column = piece.p.column,
             row = piece.p.row;
-            color = color || piece.p.color;
-    
+        color = color || piece.p.color;
+       
         if (piece && piece.p.color === color) {
             if (match.indexOf(piece) < 0) {
                 match.push(piece);
-                if (pieces.exists(column, row - 1)) {adjacentMatch(pieces.piece(column, row - 1), match, color);}
-                if (pieces.exists(column, row + 1)) {adjacentMatch(pieces.piece(column, row + 1), match, color);}
-                if (pieces.exists(column - 1, row)) {adjacentMatch(pieces.piece(column - 1, row), match, color);}
-                if (pieces.exists(column + 1, row)) {adjacentMatch(pieces.piece(column + 1, row), match, color);}
+                if (this.exists(column, row - 1)) { this.adjacentMatch(this.piece(column, row - 1), match, color); }
+                if (this.exists(column, row + 1)) { this.adjacentMatch(this.piece(column, row + 1), match, color); }
+                if (this.exists(column - 1, row)) { this.adjacentMatch(this.piece(column - 1, row), match, color); }
+                if (this.exists(column + 1, row)) { this.adjacentMatch(this.piece(column + 1, row), match, color); }
             }
         }
+    },
         
-       return match;
+    pieceAbove: function(column, row) {
+        for (var r = row; r >= 0; r--) {
+            if (this.exists(column, r)) {
+                return this.piece(column, r);
+            }
+        }
+        return null;
+    }
+
+});
+
+Q.scene("gameboard", function (stage) {
+    
+    stage.insert(new Q.Background());
+    stage.insert(new Q.LowerBound());
+    
+    var pieces = new Q.PieceCollection();
+    var fillFromAbove = function (column, row) {
+        if (row >= 0) {
+            var piece = pieces.pieceAbove(column, row);
+            if (piece) {
+                piece.p.row = row;
+                fillFromAbove(column, row - 1);
+            } else {
+                stage.insert(newPiece(c, row));
+            }
+        }
     };
 
-    var dropColumns = function() {
+    var refillColumns = function () {
         var c, r;
         for (c = 0; c < columns; c++) { //each colun
             for (r = rows - 1; r >= 0; r--) { //start from the bottom up
                 if (!pieces.exists(c, r)) {
-                    var piece = pieceAbove(c, r);
+                    var piece = pieces.pieceAbove(c, r);
                     if (piece) {
                         piece.p.row = r;
                     } else {
-                        piece = stage.insert(
-                            new Q.Piece({
-                                color: randomColor(),
-                                onTouch: destroyPiece,
-                                column: c,
-                                row: r,
-                                x: pieceSize * (c + 1/2),
-                                y: pieceSize * (r - 1/2 - rows)
-                            })
-                        );
-                        pieces.add(piece);
+                        stage.insert(newPiece(c, r));
                     }
                 }
             }
         }
     };
 
-    var pieceAbove = function(column, row) {
-        var r, p;
-        for (r = row; r >= 0; r--) {
-            if (pieces.exists(column, r)) {
-                return pieces.piece(column, r);
+    var destroyPiece = function (piece) {
+        var matches = [];
+        pieces.adjacentMatch(piece, matches);
+        if (matches.length >= minimumMatch) {
+            for (var m = matches.length - 1; m >= 0; m--) {
+                var c = matches[m].p.column, r = matches[m].p.row;
+                pieces.remove(matches[m]);
+                //fillFromAbove(c, r);
             }
         }
-        return null;
+        refillColumns();
     };
 
-    var c, r, piece;
+    var newPiece = function (column, row) {
+        var piece = new Q.Piece({
+            onTouch: destroyPiece,
+            column: column,
+            row: row,
+            x: pieceSize * (column + 1 / 2),
+            y: pieceSize * (row - 1 / 2 - rows)
+        });
+        pieces.add(piece);
+        return piece;
+    };
+
+    var c, r;
     for (c = 0; c < columns; c++) {
         for (r = 0; r < rows; r++) {
-            piece = stage.insert(new Q.Piece({
-                    color: randomColor(),
-                    onTouch: destroyPiece,
-                    column: c,
-                    row: r,
-                    x: pieceSize * (c + 1 / 2),
-                    y: pieceSize * (r - 1)
-                }));
-            //piece.p.gravity = 10;
-            pieces.add(piece);
+            stage.insert(newPiece(c, r));
         }
     }
- 
-    stage.insert(new Q.Bound({
-        x: (pieceSize * columns) / 2,
-        y: (pieceSize * rows) + 10
-    }));
-    
+
 });
 
-Q.load(["Tree.png", "red.png"], function () {
+Q.load(["red.png"], function () {
     Q.stageScene("gameboard");
 });
-
-var randomColor = function () {
-    return colors[Math.floor(Math.random() * (colors.length - 0.9))];
-};
